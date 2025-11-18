e32c8a14b2d760c552101f0645a1dc684bc0f8a7
ConPick <jun020504@gmail.com>
2025-11-18 10:38:48 +0900

    edit README, task3

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # TEAM1_12rep
+
+서윤찬 Task 4, 5
+박준우 Task 3
+조성범 Task 2
+홍윤이 Task 1
--- streamlit.py
+++ streamlit.py
@@ -1,3 +1,8 @@
 import streamlit as st
 
-st.area_chart(data=None, x=10, y=10, x_label='testX', y_label='testY', color=None, stack=None, width="stretch", height="content", use_container_width=None)
+import pandas as pd
+from numpy.random import default_rng as rng
+df = pd.DataFrame(rng(0).standard_normal((20, 3)), columns=["a", "b", "c"])
+st.area_chart(df, x_label='areaX', y_label='areaY')
+st.line_chart(df, x_label='lineX', y_label='lineY')
+st.bar_chart(df, x_label='barX', y_label='barY')
