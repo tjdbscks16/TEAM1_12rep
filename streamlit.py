3168264d6bc58d4fc072c0d54d39ed4db7b3f8c9
ConPick <jun020504@gmail.com>
2025-11-18 10:23:25 +0900

    add streamlit

--- streamlit.py
+++ streamlit.py
@@ -1,3 +1,3 @@
 import streamlit as st
 
-st.area_chart(data=None, x=10, y=10, x_label='testX', y_label='testY', color='blue', stack=None, width="stretch", height="content", use_container_width=None)
+st.area_chart(data=None, x=10, y=10, x_label='testX', y_label='testY', color=None, stack=None, width="stretch", height="content", use_container_width=None)
